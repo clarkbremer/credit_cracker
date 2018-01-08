3b75e4e44d45d5657091238c1f4a5b2241fe20b5
Clark Bremer <cbremer@ackmanndickenson.com>
2018-01-08 16:07:10 -0600

    Edit badge standards
    Incomplete.

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -69,6 +69,6 @@ class BadgesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def badge_params
-      params.require(:badge).permit(:name, :version)
+      params.require(:badge).permit(:name, :version, standard_ids: [])
     end
 end
